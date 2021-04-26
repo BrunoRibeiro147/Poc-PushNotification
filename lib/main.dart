c885f279a274377c943fd6f273129f8e85bf7795
Bruno <brf147@gmail.com>
2021-04-26 14:20:03 -0400

    :fire: Fix: Remove not long use service

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Mensagem de Notificação:',
             ),
             Text(
               '$message',
