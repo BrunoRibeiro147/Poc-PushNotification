c6ee5e7f456f6d1a2bb6486ea73f7f4c918addb5
Bruno <brf147@gmail.com>
2021-04-26 09:48:20 -0400

    First try with Cloud Messaging

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final FirebaseMessaging _fcm = FirebaseMessaging.instance;
+  String? message;
+
+  _registerOnFirebase() {
+    _fcm.subscribeToTopic('all');
+    _fcm.getToken().then((token) => print(token));
+  }
+
+  @override
+  void initState() {
+    _registerOnFirebase();
+    getMessage();
+    super.initState();
+  }
+
+  void getMessage() {
+    FirebaseMessaging.onMessage.listen((RemoteMessage remoteMessage) {
+      print('Message receive $remoteMessage');
+      setState(() => message = remoteMessage.notification!.body);
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage remoteMessage) {
+      print('Message receive $remoteMessage');
+      setState(() => message = remoteMessage.notification!.body);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$message',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
